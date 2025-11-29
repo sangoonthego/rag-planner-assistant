798f952bd17b8f06da8b67bd5d9332300e82414b
Nguyen Ngoc <knkidngoc@gmail.com>
2025-11-29 14:26:11 +0700

    update

--- src/core/document_loader.py
+++ src/core/document_loader.py
@@ -19,17 +19,17 @@ def load_documents() -> list[Document]:
             for doc in docs:
                 doc.metadata['type'] = 'plan_document'
             all_documents.extend(docs)
-            print(f"Đã tải file DOCX: {filename}")
+            print(f"Downloaded file DOCX: {filename}")
             
         elif filename.endswith(".xlsx"):
             excel_docs = convert_excel_to_text(file_path)
             all_documents.extend(excel_docs)
-            print(f"Đã xử lý file XLSX: {filename}")
+            print(f"Handling file XLSX: {filename}")
             
         elif filename.endswith(".txt"):
             loader = TextLoader(file_path)
             all_documents.extend(loader.load())
-            print(f"Đã tải file TXT: {filename}")
+            print(f"Downloaded file TXT: {filename}")
             
         
     print(f"\nDownloaded all {len(all_documents)} raw Document.")
