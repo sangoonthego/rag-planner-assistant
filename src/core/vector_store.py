0a4546699388a70666a55029a776e746f6787727
Nguyen Ngoc <knkidngoc@gmail.com>
2025-11-29 14:25:37 +0700

    fix: fix logic store db

--- src/core/vector_store.py
+++ src/core/vector_store.py
@@ -1,7 +1,7 @@
 from langchain_community.vectorstores import Chroma
-# from langchain.schema import Document
 from langchain_core.documents import Document
 from .embedding_model import get_embedding_model
+import os
 
 PERSIST_DIRECTORY = "./vector_db"
 
@@ -10,18 +10,15 @@ def initialize_vector_store(documents: list[Document] = None):
     
     if documents:
         print(f"Starting to embed and store {len(documents)} para in ChromaDB...")
-        
         vectordb = Chroma.from_documents(
             documents=documents,
             embedding=embedding_model,
             persist_directory=PERSIST_DIRECTORY
         )
-        vectordb.persist()
-        print(f"Vector Store Saved in folder: {PERSIST_DIRECTORY}")
         return vectordb
     else:
-        print(f"Download existed Vector Store from: {PERSIST_DIRECTORY}")
+        print(f"Load Vector Store that existed from: {PERSIST_DIRECTORY}")
         return Chroma(
             persist_directory=PERSIST_DIRECTORY,
-            embedding=embedding_model
+            embedding_function=embedding_model 
         )
